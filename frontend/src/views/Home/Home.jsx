0e75764675612da5b21da49ee4362441513b3271
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-03-21 09:14:51 -0300

    numbers fixde

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -33,6 +33,13 @@ const Home = () => {
             // backgroundAttachment: "fixed",
           }}
         ></div>
+                <div className="w-full flex justify-center items-center px-0 lg:px-10 ">
+          <h1 className="font-lato-700 text-3xl lg:text-5xl  text-emerald-800 text-center">
+            Somos un estudio con 30 años de trayectoria de labor profesional en
+            Arquitectura, y más de 300.000 m2 construidos.
+          </h1>
+        </div>
+        <Counter />
       </div>
       <div className="flex flex-wrap justify-center items-center bg-gray-100 space-y-8 lg:space-y-10 py-8 lg:py-10 px-4 lg:px-10 xl:px-20">
         {/* <div className="w-full flex flex-wrap justofy-center lg:justify-end items-center px-0 lg:px-12">
